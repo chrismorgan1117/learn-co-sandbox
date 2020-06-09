7170254cd10ef97901c3de95d3c2fa300f8e7393
Christopher Morgan <cwm111794@gmail.com>
2020-06-09 02:36:39 +0000

    Automatically backed up by Learn

---
+++ if-end-statements.rb
@@ -0,0 +1,49 @@
+#run_code_inside = false
+#puts"Code before if...end"
+#if run_code_inside
+ # puts"Code inside"
+#end 
+#puts "Code after if...End"
+#chance_of_rain=-23
+#if chance_of_rain <= 0.25
+  #puts "Pack a sun shelter"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  #puts "Pack an umbrella"
+#else
+#puts "Stay home and read hegel"
+#end 
+#puts "you know what year it is??"
+#this_year = 
+#puts "hey, its 2019" unless 
+#this_year == 2019
+
+#name = "Tis I"
+#case name 
+
+#when "Alice"
+ # puts "Hello, Alice!"
+  #when "The White Rabbit"
+   # puts "Dont be late"
+    #when "The Mad Hatter"
+     # puts "Welcome to the tea party, Mad Hatter"
+      #when "The Queen of Hearts"
+       # puts "Please dont chop off my head"
+      #else
+       # puts "whoooooo are you??"
+      #end 
+      
+   #   count = 0 
+    #  while count <= 3 do 
+     
+    #   puts "I am the #{count}, I love to count"
+     #   count += 1 
+      #end 
+      
+     # magic_exit_number = 7
+#count = 0
+#while count < 10 do
+  #break if count == magic_exit_number
+  #puts "I am the #{count}, I love to count!" # Work
+  #count = count + 1
+#end
+
